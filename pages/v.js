53069f20f53c2e2035111a8ddc3eb84578b15458
Cosmin Serdean <cosmoserdean@gmail.com>
2018-01-24 12:52:50 +0200

    Verify login page

--- pages/v.js
+++ pages/v.js
@@ -1,5 +1,24 @@
 import React from 'react'
 
 export default ({ url: { query: { valid } } }) => {
-	return <h1>{valid ? 'yay' : 'nay'}</h1>
+	return (
+		<div className="container">
+			<h2 className="title">
+				{valid ? 'Logged in successfully.' : 'Invalid token.'}
+			</h2>
+			{valid && <p>You may close this window.</p>}
+			<style jsx>{`
+				.title {
+					font-weight: normal;
+				}
+				.container {
+					height: 100vh;
+					display: flex;
+					justify-content: center;
+					align-items: center;
+					flex-direction: column;
+				}
+			`}</style>
+		</div>
+	)
 }
